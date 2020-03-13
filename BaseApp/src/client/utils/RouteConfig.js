b6f1ea370703de87a49e30ca88e36cdad4d7b830
nanugantiep <nanuganti@ebsco.com>
2020-03-13 10:34:07 -0400

    deleted not needed routes

--- BaseApp/src/client/utils/RouteConfig.js
+++ BaseApp/src/client/utils/RouteConfig.js
@@ -1,24 +1,4 @@
 export default {
-    "/": {
-        name: 'Product',
-        url: 'http://localhost:8082'
-    },
-    "/product": {
-        name: 'Product',
-        url: "http://localhost:8082"
-    },
-    "/cart": {
-        name: 'Cart',
-        url: "http://localhost:8083"
-    },
-    "/core": {
-        name: 'Core',
-        url: "http://localhost:8081"
-    },
-    "/results": {
-        name: 'ResultList',
-        url: "http://localhost:8084"
-    },
     "/topic": {
         name: 'Topic',
         url: "https://haritest-micro-front-end-assets.s3.amazonaws.com/topic"
